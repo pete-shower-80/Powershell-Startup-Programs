b723b43de1a52ad3cf2e9efb7e3ca47a42204495
pete-shower-80 <137993092+pete-shower-80@users.noreply.github.com>
2024-10-28 13:07:26 +0100

    Update autoStartPrograms.bat

--- autoStartPrograms.bat
+++ autoStartPrograms.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" powershell -WindowStyle Hidden -File "C:\Users\Student\OneDrive - GFN GmbH (EDU)\Dokumente\FISI-GFN\gfn dokumente\Schulungsmaterial\LF-VT2\aktive PS-Skripte\AutostartProgramsv2.ps1"
+start "" powershell -WindowStyle Hidden -File "##path to your .ps1 script"
